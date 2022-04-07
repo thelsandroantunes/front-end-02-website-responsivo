f2ef5e8d1fc3119928d907e1611ba3009a07ba7c
Thelsandro <tca.eng17@uea.edu.br>
2022-04-06 23:08:00 -0400

    last

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ==========================*/
+var typed = new Typed(".typed", {
+  strings: ["","Back End Developer", "Analista de Dados", "Estudante"],
+  typedSpeed: 5000,
+  BackSpeed: 2500,
+  loop:true
+})
